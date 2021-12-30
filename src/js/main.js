df1d09de86b244348dc0d78586b76fd5c3a6e291
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-30 15:08:01 +0100

    added new functions to get favourites, still fighting to get a positive result

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,13 @@ let input = document.querySelector('.js-input');
 const searchButton = document.querySelector('.js-searchButton');
 let searchResultList = document.querySelector('.searchresults');
 const favouriteSelector = document.querySelectorAll('.results') //parte 3
+const favouriteListOfAnimes = document.querySelector('.searchresults__favourites'); //aside de parte 3 para pintar favoritos
 //Variables globales
 const alternativeImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 let userInput = '';
 let animeSeriesArray = [];
 let favouriteSeriesArray = [];
+
 
 
 
@@ -35,18 +37,32 @@ function fetchDataAnime() {
                     //No se me pinta la foto alternativa
                     const animeSerie = animeSeriesArray[i];
                     if (animeSerie.img_url === null) {
-                        searchResultList.innerHTML += `<article class="results"> <p><h3>${animeSerie.title}</h3></p><img class="image" src="${alternativeImage}"/></article>`
+                        searchResultList.innerHTML += `<article class="results id=${animeSerie.id}> <p><h3>${animeSerie.title}</h3></p><img class="image" src="${alternativeImage}"/></article>`
 
                     } else {
 
-                        searchResultList.innerHTML += `<article class="results"> <p><h3>${animeSerie.title}</h3></p><img class="image" src="${animeSerie.image_url}"/></article>`
+                        searchResultList.innerHTML += `<article class="results" id=${animeSerie.id}> <p><h3>${animeSerie.title}</h3></p><img class="image" src="${animeSerie.image_url}"/></article>`
                     }
-                    //Añado Listener al article de reultados
+                    //Parte III Añado Listener al article de resultados
+
                     const favouriteSelector = document.querySelectorAll('.results')
 
                     for (const fav of favouriteSelector) {
-                        fav.addEventListener('click', handleFavourites)
+                        fav.addEventListener('click', global) //la función que quiero usar es global, pero no funciona
                     }
+
+                    ///Parte III. Pintar favoritos
+
+                    /*for (let i = 0; i < favouriteSeriesArray.length; i++) {
+                        const favouriteSeries = favouriteSeriesArray[i]
+                        favouriteListOfAnimes.innerHTML += `<article class="results" id=${animeSerie.mal_id}> <p><h3>${favouriteSeries.title}</h3></p><img class="image" src="${favouriteSeries.image_url}"/></article>`
+
+
+
+                    }*/
+
+
+
 
 
                 }
@@ -54,17 +70,42 @@ function fetchDataAnime() {
 
 
         })
-    //Función manejadora los favoritos
-    function handleFavourites(event) {
-        //console.log('holi');
-        const favouriteAnime = event.currentTarget;
+    //Parte III. Función manejadora  de favoritos
+    function handleFavourites(event) { //Función para poder hacer un nuevo array con los ids de las series. FUNCIONA
+
+        const favouriteAnime = event.currentTarget.id;
         console.log(favouriteAnime);
-        favouriteAnime.classList.toggle('favourite');
+
+
+    }
+    function changeColorFavourite(event) { //Función para cambiar el color al favorito FUNCIONA
+
+        const favouriteAnime = event.currentTarget;
+        favouriteAnime.classList.toggle('favourite')
+
+    }
+    function global() { //Esta función es la que quiero usar en el listener, pero las dos funcionas juntas dan error en el listener.NO FUNCIONA
+
+        handleFavourites();
+        changeColorFavourite();
 
     }
 
 
+
+
+    //Añado las series seleccionadas al array vacío que he creado
+    /*if (favouriteAnime.classList.contains('favourite')) {
+        favouriteSeriesArray.push(favouriteAnime);
+        console.log(favouriteSeriesArray);
+    }*/
+
+
 }
+
+
+
+
 
 //Listeners
 
