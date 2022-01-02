270ec6ab27a4ca55ac8ae6f94117b733a7b4f0d1
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-02 18:19:54 +0100

    added functuon render favoirties and localStirage)

--- src/js/main.js
+++ src/js/main.js
@@ -1,128 +1,130 @@
-'use strict';
+"use strict";
 
 //Traigo del HTML
 
-let input = document.querySelector('.js-input');
-const searchButton = document.querySelector('.js-searchButton');
-let searchResultList = document.querySelector('.searchresults');
-const favouriteSelector = document.querySelectorAll('.results') //parte 3
-const favouriteListOfAnimes = document.querySelector('.searchresults__favourites'); //aside de parte 3 para pintar favoritos
+let input = document.querySelector(".js-input");
+const searchButton = document.querySelector(".js-searchButton");
+let searchResultList = document.querySelector(".searchresults");
+const favouriteSelector = document.querySelectorAll(".results"); //parte 3
+const favouriteListOfAnimes = document.querySelector(
+    ".searchresults__favourites"
+); //aside de parte 3 para pintar favoritos
 //Variables globales
-const alternativeImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-let userInput = '';
+const alternativeImage =
+    "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+let userInput = "";
 let animeSeriesArray = [];
 let favouriteSeriesArray = [];
-
-
-
-
-
 
 //Empezamos con el fetch... Partes I y II
 function fetchDataAnime() {
     searchResultList.innerHTML = "";
 
     fetch(`https://api.jikan.moe/v3/search/anime?q=${input.value}`)
-
         .then((response) => response.json())
         .then((dataAnime) => {
             animeSeriesArray = dataAnime.results;
             //console.log(animeSeriesArray);
 
-
-
-
             if (animeSeriesArray.length === 0) {
-                searchResultList.innerHTML += `<p class="paragraph">No existe este título. Prueba con otro</p>`
+                searchResultList.innerHTML += `<p class="paragraph">No existe este título. Prueba con otro</p>`;
             } else
                 for (let i = 0; i < animeSeriesArray.length; i++) {
                     //No se me pinta la foto alternativa
                     const animeSerie = animeSeriesArray[i];
 
-
                     if (animeSerie.img_url === null) {
-                        searchResultList.innerHTML += `<article class="results" data-id="${animeSerie.mal_id}"> <p><h3 data-title="${animeSerie.title}class="searchresults__title--js" >${animeSerie.title}</h3></p><img class="image" src="${alternativeImage}"/></article>`
-
+                        searchResultList.innerHTML += `<article class="results" data-id="${animeSerie.mal_id}"> <p><h3 data-title="${animeSerie.title}class="searchresults__title--js" >${animeSerie.title}</h3></p><img class="image" src="${alternativeImage}"/></article>`;
                     } else {
-
-                        searchResultList.innerHTML += `<article class="results" data-id="${animeSerie.mal_id}"> <p><h3 data-title="${animeSerie.title}" class= "searchresults__title--js">${animeSerie.title}</h3></p><img class="image" src="${animeSerie.image_url}"/></article>`
+                        searchResultList.innerHTML += `<article class="results" data-id="${animeSerie.mal_id}"> <p><h3 data-title="${animeSerie.title}" class= "searchresults__title--js">${animeSerie.title}</h3></p><img class="image" src="${animeSerie.image_url}"/></article>`;
                     }
-
-
-
 
                     //Parte III Añado Listener al article de resultados
 
-                    const favouriteSelector = document.querySelectorAll('.results')
+                    const favouriteSelector = document.querySelectorAll(".results");
 
                     for (const fav of favouriteSelector) {
-                        fav.addEventListener('click', globalFunction)
+                        fav.addEventListener("click", globalFunction);
                     }
                 }
-
-
-        }
-        )
-
+        });
 }
 
-
-function changeColorFavourite(event) { //Función para cambiar el color a la serie seleccionada como favorita
+function changeColorFavourite(event) {
+    //Función para cambiar el color a la serie seleccionada como favorita
 
     const favouriteAnime = event.currentTarget;
-    favouriteAnime.classList.toggle('favourite')
+    favouriteAnime.classList.toggle("favourite");
     //console.log(event)
+}
+
+
+
+
+function handleFavourites(event) {
+    //Función para poder hacer un nuevo array con los ids de las series y subirlos a favoritos
+
+
+
+
+
+    const favouriteSeries = parseInt(event.currentTarget.dataset.id);
+    let favouriteIndex = favouriteSeriesArray.findIndex(
+        (serie) => serie.mal_id === favouriteSeries
+    );
+
+    if (favouriteIndex >= 0) {
+        favouriteSeriesArray.splice(favouriteIndex, 1);
+    } else {
+        const searchAnimeObject = animeSeriesArray.find((serie) => serie.mal_id === favouriteSeries)
+        favouriteSeriesArray.push(searchAnimeObject)
+    }
+    console.log(favouriteSeriesArray);
+    localStorage.setItem("favourites", JSON.stringify(favouriteSeriesArray));
+
 
 }
-function handleFavourites(event) { //Función para poder hacer un nuevo array con los ids de las series. 
 
-    const favouriteSeries = parseInt(event.currentTarget.dataset.id); //
-    let favourites = animeSeriesArray.find((serie) => serie.mal_id === favouriteSeries)
-    //Aquí hago un push para subirme los datos de let favourites (no sé si hacía falta)
-    favouriteSeriesArray.push(favourites);
-    //console.log(favouriteSeriesArray);
-}
+
+
+
 function renderFavourites() {
-    //Ahora quiero pintar una lista con los favoritos. Que Dios reparta suerte! NO SALE TODAVÍA
+    //Ahora quiero pintar una lista con los favoritos.
     favouriteListOfAnimes.innerHTML = "";
     for (let i = 0; i < favouriteSeriesArray.length; i++) {
-
-        favouriteListOfAnimes.innerHTML += `<article class="results" > <p><h3 class= "searchresults__title">${favouriteSeriesArray[i].title}</h3></p><img class="image" src="${favouriteSeriesArray[i].image_url}"/></article>`
-
-
-
+        favouriteListOfAnimes.innerHTML += `<article class="results" > <p><h3 class= "searchresults__title">${favouriteSeriesArray[i].title}</h3></p><img class="image" src="${favouriteSeriesArray[i].image_url}"/></article>`;
     }
 }
-function deleteFavourites() { //Con esta funcion quito la clase de favoritos y se va el color, pero se me sige agregando a la lista de favoritos
+//Con esta función quiero borrar favoritos de mi lista de favoritos NO FUNCIONA: sobreescribe y no elimina y de index sale -1 por el parent.Node
+
+
+function deleteFavourites() {
+    //Con esta funcion quito la clase de favoritos y se va el color, pero se me sige agregando a la lista de favoritos
     for (let i = 0; i < favouriteSeriesArray; i++) {
-
-
-        if (favouriteSeriesArray[i].contains('favourite')) {
-            favouriteSeriesArray[i].classList.remove('favourite');
-
+        if (favouriteSeriesArray[i].contains("favourite")) {
+            favouriteSeriesArray[i].classList.remove("favourite");
         }
     }
 }
+function getStorageData() {
+    const seriesStoraged = JSON.parse(localStorage.getItem("favourites"));
+    if (seriesStoraged !== null) {
+        favouriteSeriesArray = seriesStoraged
+    }
 
-
-
-
-
-
-function globalFunction(event) { //Esta función es la que quiero uso en el listener "fav" para cambiar el color, mostrar el array con favoritos y pintar los favs
-
-    handleFavourites(event);
-    changeColorFavourite(event);
     renderFavourites()
-    deleteFavourites()
 
 
 }
 
-
-
-
-
+function globalFunction(event) {
+    //Esta función es la que quiero uso en el listener "fav" para cambiar el color, mostrar el array con favoritos y pintar los favs
+    handleFavourites(event);
+    changeColorFavourite(event);
+    renderFavourites();
+    deleteFavourites();
+    // removeFromFavourites(event);
+}
 
 
 
@@ -130,18 +132,7 @@ function globalFunction(event) { //Esta función es la que quiero uso en el list
 
 //Listeners
 
-searchButton.addEventListener('click', fetchDataAnime);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+searchButton.addEventListener("click", fetchDataAnime);
+getStorageData();
