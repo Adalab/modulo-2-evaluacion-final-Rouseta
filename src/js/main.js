ae99cd956eefa956466f74df57c67414fd402754
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-04 11:33:01 +0100

    added explanatory comments

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,6 @@ let favouriteSeriesArray = [];
 //Empezamos con el fetch
 function fetchDataAnime(event) {
     event.preventDefault();
-    //searchResultList.innerHTML = "";
 
     fetch(`https://api.jikan.moe/v3/search/anime?q=${input.value}`)
         .then((response) => response.json())
@@ -27,23 +26,20 @@ function fetchDataAnime(event) {
             animeSeriesArray = dataAnime.results;
 
             renderResults();
-
-
-
         });
 }
 
 //Funciones
 
 function renderResults() {
-
     //Función que pinta los resultados de la búsqueda
     searchResultList.innerHTML = "";
 
     if (animeSeriesArray.length === 0) {
         searchResultList.innerHTML += `<p class="paragraph">No existe este título. Prueba con otro</p>`;
     } else {
-        for (let i = 0; i < animeSeriesArray.length; i++) {//Recorro el array
+        for (let i = 0; i < animeSeriesArray.length; i++) {
+            //Recorro el array
             const animeSerie = animeSeriesArray[i];
 
             //Esto se hace para ver si está o no está en mis favoritos guardados, se recorre el array de favouriteSeriesArray
@@ -51,11 +47,11 @@ function renderResults() {
                 (serie) => serie.mal_id === animeSerie.mal_id
             );
 
-
             // Si están  se  pinta en la lista de resultados, el resultado con la bubble( que es la clase .favourite)
             if (favouriteIndex >= 0) {
                 searchResultList.innerHTML += `
-                <article class="results favourite" data-id="${animeSerie.mal_id}">
+                <article class="results favourite" data-id="${animeSerie.mal_id
+                    }">
                     <h3 data-title="${animeSerie.title
                     } class="searchresults__title--js" >
                         ${animeSerie.title}
@@ -105,11 +101,11 @@ function handleFavourites(event) {
 
     const favouriteSeries = parseInt(event.currentTarget.dataset.id);
     let favouriteIndex = favouriteSeriesArray.findIndex(
-
         (serie) => serie.mal_id === favouriteSeries
     );
 
-    if (favouriteIndex >= 0) { //Si coincide que el elemento clickado está en el array, entonces lo corto
+    if (favouriteIndex >= 0) {
+        //Si coincide que el elemento clickado está en el array, entonces lo corto
         favouriteSeriesArray.splice(favouriteIndex, 1);
     } else {
         //Si no coincide, es que no está guardado en mi array y lo quiero guardar con push. Ahora ahago un find porque necesito buscar el objeto para pintarlo
@@ -119,9 +115,7 @@ function handleFavourites(event) {
         favouriteSeriesArray.push(searchAnimeObject);
     }
     //Guardo mis favoritos en el almacenamiento local
-    saveData()
-
-
+    saveData();
 }
 
 function renderFavourites() {
@@ -140,16 +134,12 @@ function renderFavourites() {
         `;
     }
 
-
-
-
     //Listener al botón x que borra el favorito seleccionado
     const xButtons = document.querySelectorAll(".deleteOneBtn");
     for (const xButton of xButtons) {
         xButton.addEventListener("click", resetOne);
     }
 }
-
 
 function deleteFavourites() {
     //Con esta función quito la clase de favoritos y se intercambia el color del título y fondo.
@@ -176,33 +166,31 @@ function resetAll() {
 }
 function resetOne(event) {
     //Borrar un favorito
-
+    //Recorro nuevamente el array para buscar si está o no está la serie que quiero borrar, sobre la que se ha hecho click
     const deletedSeries = parseInt(event.currentTarget.dataset.id);
     let deletedIndex = favouriteSeriesArray.findIndex(
-
-
         (deleted) => deleted.mal_id === deletedSeries
     );
 
     if (deletedIndex >= 0) {
+        //Si la serie en la que hago click está en el array, entonces esa es la que quiero borrar y la corto de la lista
         favouriteSeriesArray.splice(deletedIndex, 1);
     }
 
-    localStorage.setItem("favourites", JSON.stringify(favouriteSeriesArray));
-
+    //Llamo a la función saveData (localStorage) para actualizar los datos de almacenamiento
+    saveData();
+    //Llamo a la función render Favourites para pintar la lista  actualizada
     renderFavourites();
-    renderResults()
+    //LLamo a la función de renderResults para pintar la lsita de resultados actualizada (para que se borre la bubble)
+    renderResults();
 }
-
-
-
 
 function globalFunction(event) {
     //Esta función es la que quiero uso en el listener "fav" para cambiar el color, mostrar el array con favoritos y pintar los favs
     handleFavourites(event);
     changeColorFavourite(event);
     renderFavourites();
-    renderResults()
+    renderResults();
     deleteFavourites();
 }
 function saveData() {
@@ -215,4 +203,3 @@ searchForm.addEventListener("submit", fetchDataAnime);
 resetButton.addEventListener("click", resetAll);
 
 getStorageData();
-
